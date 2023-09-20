1e0fed2728f0d018ecc271ef8a11f4405d4fdd61
Jorge Gonzalez <jorge.gonzalez@reactivemarkets.com>
2023-09-20 19:45:51 +0200

    sort by tag or name, minor improvements

--- fix2pipe++.py
+++ fix2pipe++.py
@@ -27,24 +27,39 @@ def parse_tags_file(tags_filename):
     return tags_map
 
 
-def print_fix_msg(msg_map, tags_map, direction):
-    for key, value in sorted(msg_map.items()):
-        description = None
+def tag_key(t):
+    return t[0]
+
+
+def name_key(t):
+    return t[1]
+
+
+def print_fix_msg(msg_map, tags_map, direction, sort_by):
+    msg_fields = []
+    for key, value in msg_map.items():
+        name = ""
         if not tags_map is None and key in tags_map.keys():            
-            description = tags_map[key]
-        if description is None:
-            description = ""
+            name = tags_map[key]
+        msg_fields.append((key, name, value))
+
+    if sort_by == 'name' and not tags_map is None:
+        msg_fields.sort(key=name_key)
+    else:
+        msg_fields.sort(key=tag_key)
+
+    for key, name, value in msg_fields:
         if direction == 'incoming':
             color_str = colors.GREEN
         else:
             color_str = colors.BLUE
-        output = "{0:6} {1:28} {2}".format(key, description, value)
+        output = "{0:6} {1:28} {2}".format(key, name, value)
         print(color_str + output + colors.ENDCOLOR)
 
 
-def parse_fix_msg(msg, tags_map, direction):
+def parse_fix_msg(msg, tags_map, direction, sort_by):
     msg_map = {}
-    fields = msg.split('\x01')
+    fields = msg.split('|')
     for field in fields:
         if len(field) > 0:
             tag_value = field.split('=')
@@ -52,17 +67,20 @@ def parse_fix_msg(msg, tags_map, direction):
             value = tag_value[1]
             msg_map[int(tag)] = value
 
-    print_fix_msg(msg_map, tags_map, direction)
+    print_fix_msg(msg_map, tags_map, direction, sort_by)
     
 
-def parse_line(line, tags_map, only_fix):
+def parse_line(line, tags_map, only_fix, sort_by):
+    line = line.replace('\x01', '|')
     result = re.search('.+<.+->.+,.+> (incoming|outgoing): (.*)', line)
-    if not only_fix:            
+
+    if not only_fix:
         print(line, end="")
     
     if result:
-        msg = result.groups()[1]        
-        parse_fix_msg(msg, tags_map, result.groups()[0])
+        direction = result.groups()[0]
+        msg = result.groups()[1]
+        parse_fix_msg(msg, tags_map, direction, sort_by)
         if only_fix:        
             print('------------------------------------------------------')
 
@@ -73,6 +91,7 @@ if __name__ == "__main__":
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-t", "--tags", help="path to Tag.hpp")
     parser.add_argument("-o", "--only-fix", action="store_true", help="only outputs formatted fix")
+    parser.add_argument("-s", "--sort-by", choices=['tag', 'name'], default='tag', help="sorts fields by tag number or name")
 
     args = parser.parse_args()
     config = vars(args)
@@ -82,6 +101,7 @@ if __name__ == "__main__":
         tags_map = parse_tags_file(config['tags'])
     
     only_fix = config['only_fix']
+    sort_by = config['sort_by']
 
     for line in sys.stdin:
-        parse_line(line, tags_map, only_fix)
+        parse_line(line, tags_map, only_fix, sort_by)
